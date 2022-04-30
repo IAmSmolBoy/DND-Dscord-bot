1ef8d44649bf93adadc31e2ecbb35ff1e1a2bfa7
SmolBoy <65689332+IAmSmolBoy@users.noreply.github.com>
2022-04-30 15:59:12 +0800

    Mising Access Error

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,10 @@
 require("./init_mongodb")
 require("dotenv").config()
 
-const { Client, Intents } = require("discord.js"), Task = require("./models/task"), rr = require("./models/rr")
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS, "GUILDS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", 
+const { Client } = require("discord.js"), Task = require("./models/task"), rr = require("./models/rr")
+const client = new Client({ intents: [ "GUILDS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", 
 "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", 
-"DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING", "GUILD_SCHEDULED_EVENTS"], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
+"DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING", "GUILD_SCHEDULED_EVENTS" ], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 const commands = require("./commands")
 var prefix = "$"
 const commandDict = {
@@ -142,13 +142,16 @@ client.on("ready", async () => {
     setInterval(async () => {
         const tasks = await Task.find()
         tasks.forEach(async (e) => {
-            const SmolBoyServ = await client.guilds.fetch(e.guild)
-            const taskChannel = await SmolBoyServ.channels.fetch(e.channel), todayDate = new Date()
-            todayDate.setHours(todayDate.getHours() + 8)
-            if (todayDate >= e.dateTime && todayDate.getTime() >= e.dateTime.getTime()) {
-                taskChannel.send(`${e.role}, ${e.msgContent}`)
-                await Task.deleteOne(e)
+            try {
+                const SmolBoyServ = await client.guilds.fetch(e.guild)
+                const taskChannel = await SmolBoyServ.channels.fetch(e.channel), todayDate = new Date()
+                todayDate.setHours(todayDate.getHours() + 8)
+                if (todayDate >= e.dateTime && todayDate.getTime() >= e.dateTime.getTime()) {
+                    taskChannel.send(`${e.role}, ${e.msgContent}`)
+                    await Task.deleteOne(e)
+                }
             }
+            catch (err) {}
         })
     }, 10000);
 })
